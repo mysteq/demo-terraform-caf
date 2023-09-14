eee11fdc60942af903133aa67e1b608e2862d3f4
Ketil Gjerde <raideer@gmail.com>
2023-09-14 09:09:38 +0200

    chore: update
    Signed-off-by: Ketil Gjerde <raideer@gmail.com>

--- 01_core/providers.tf
+++ 01_core/providers.tf
@@ -6,6 +6,7 @@ terraform {
     }
   }
   backend "azurerm" {
+    subscription_id      = "87e43d6c-337a-44f8-b908-c4b12dd914a9"
     resource_group_name  = "rg-tfstate-01"
     storage_account_name = "tfstatecaf453523"
     container_name       = "tfstatecaf453523"
@@ -16,5 +17,5 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "87e43d6c-337a-44f8-b908-c4b12dd914a9"
-  tenant_id = "2044f836-f4df-477c-a129-cfa16a9c16cd"
+  tenant_id       = "2044f836-f4df-477c-a129-cfa16a9c16cd"
 }
